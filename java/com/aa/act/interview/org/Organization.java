db311ff4879874732cc6ef77ae3552773f34535e
RenegadeGasper <ikkaku354@gmail.com>
2023-07-03 12:25:09 -0700

    Completed the hire() method

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 public abstract class Organization {
 
     private Position root;
+    private int indexCounter;
     
     public Organization() {
         root = createOrganization();
+        indexCounter = 1;
     }
     
     protected abstract Position createOrganization();
@@ -21,9 +23,37 @@ public abstract class Organization {
      */
     public Optional<Position> hire(Name person, String title) {
         //your code here
-
+        Optional<Position> position = findPositionbyTitle(root,title);
+        if(position.isPresent()){
+            Position foundPosition = position.get();
+            if (!foundPosition.isFilled()){
+                int counter = index();
+                Employee employee = new Employee(counter, person);
+                foundPosition.setEmployee(Optional.of(employee));
+                return Optional.of(foundPosition);
+            }
+        }
         return Optional.empty();
     }
+
+    private int index(){
+        return indexCounter++;
+    }
+
+    private Optional<Position> findPositionbyTitle(Position position, String title){
+        if (position.getTitle().equals(title)){
+            return Optional.of(position);
+        }
+
+        for (Position directReport : position.getDirectReports()){
+            Optional<Position> foundPosition = findPositionbyTitle(directReport, title);
+            if(foundPosition.isPresent()){
+                return foundPosition;
+            }
+        }
+        return Optional.empty();
+    }
+
 
     @Override
     public String toString() {
